9530c92140fc114d7f3b7cf48d33c36d4eb9f55c
DanilGolovin <danyk612345@gmail.com>
2019-08-28 23:03:28 +0300

    Extended weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
-const  forecast = (latitude, longitude, callback) => {
-    const  url = 'https://api.darksky.net/forecast/a71b24182468382281a48b420b3adcbe/' + encodeURIComponent(longitude) + ',' + encodeURIComponent(latitude)
-    request({url, json: true}, (error, {body}) => {
-        if (error){
-            callback('Unable to connect to location services', undefined)
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/a71b24182468382281a48b420b3adcbe/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)+'?lang=ru&units=si'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'temperature : ' + body.currently.temperature + ', rainchance : ' + body.currently.precipProbability
-            )
+            callback(undefined, body.daily.data[0].summary + '; температура - ' + body.currently.temperature + '; макс. температура - ' + body.daily.data[0].temperatureHigh + '; минималная температура - ' + body.daily.data[0].temperatureLow + '; скорость ветра - ' + body.currently.windSpeed +' м/с; вероятность дождя - ' + body.currently.precipProbability + '%; УФ индекс - ' + body.currently.uvIndex)
         }
     })
 }
